e962f48a532445113a4094c0ce18e439e25cb3a6
Jide Oyelayo <JideOyelayo1@Gmail.com>
2023-02-12 13:41:27 +0000

    i know how to navi now YAYYYY

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, Button,SafeAreaView } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, Button,SafeAreaView,Image } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 
 export default function Home({ navigation }) {
 
   const toReviewDetails = () => {    navigation.push('ReviewDetails');  }
+  const toLoginPage = () => {    navigation.push('LoginPage');  }
   const toAbout = () => {    navigation.push('About');  }
   const toCalc = () => {    navigation.push('Calculator');  }
   const toProductList = () => {    navigation.push('Product_List');  }
@@ -13,13 +15,18 @@ export default function Home({ navigation }) {
 
   return (
     <SafeAreaView style={globalStyles.container} >
-      <Text style={globalStyles.titleText}>Home Screen</Text>
+      <StatusBar style="auto" />
+      <Image source={require("../assets/logo-no-background.png")}
+      style={{width: "100%", height:160}} 
+      />
+      
       <Button title='About' onPress={toAbout} />
+      <Button title='Login' onPress={toLoginPage} />
       <Button title='Review' onPress={toReviewDetails} />
       <Button title='Calculator' onPress={toCalc} />
       <Button title='Product List' onPress={toProductList} />
       <Button title='Contacts' onPress={toContacts} />
-      <Text> app!</Text>
+      <Text> Still Under development</Text>
     </SafeAreaView>
   );
 }
